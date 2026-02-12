815ea7a3391d5a4d623f78ee8f849166a7975c57
Fevzi FİDAN <fevzifidan05@gmail.com>
2026-02-13 00:34:36 +0300

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <h1>CoLDAT Project</h1>
+      <p>Senior Design Project</p>
+    </div>
+  )
+}
+
+export default App
