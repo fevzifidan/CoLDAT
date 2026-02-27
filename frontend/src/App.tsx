323d89c0e9360ddf5aeea0e32f40a12e25731bc5
Fevzi FİDAN <fevzifidan05@gmail.com>
2026-02-27 23:05:00 +0300

    App.tsx updated, some extension typos solved.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,25 @@
 import './App.css';
 // import { useTranslation } from "react-i18next";
+import { AuthProvider } from './context/AuthContext';
 import { Toaster } from "@/components/ui/sonner";
-import { BrowserRouter, Routes } from 'react-router-dom';
-import AxiosInterceptorSetup from './components/custom/AxiosInterceptorSetup/AxiosInterceptorSetup';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/custom/ProtectedRoute/ProtectedRoute';
+import AxiosInterceptorSetup from './components/custom/AxiosInterceptorSetup/AxiosInterceptorSetup.jsx';
 
 function App() {
   // const { t, i18n } = useTranslation();
 
   return (
-    <>
-      <Toaster position="top-right" richColors />
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
         <AxiosInterceptorSetup />
+        <Toaster position="top-right" richColors />
+
         <Routes>
           <></>
         </Routes>
-      </BrowserRouter>
-    </>
+      </AuthProvider>
+    </BrowserRouter>
   )
 }
 
