eb7da652107312d8ba8ea3a76b309bd2c8e3924d
Fevzi FİDAN <fevzifidan05@gmail.com>
2026-02-27 22:34:17 +0300

    AxiosInterceptorSetup added, App.tsx updated.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
-import './App.css'
+import './App.css';
 // import { useTranslation } from "react-i18next";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
+import { BrowserRouter, Routes } from 'react-router-dom';
+import AxiosInterceptorSetup from './components/custom/AxiosInterceptorSetup/AxiosInterceptorSetup';
 
 function App() {
   // const { t, i18n } = useTranslation();
@@ -8,6 +10,12 @@ function App() {
   return (
     <>
       <Toaster position="top-right" richColors />
+      <BrowserRouter>
+        <AxiosInterceptorSetup />
+        <Routes>
+          <></>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
