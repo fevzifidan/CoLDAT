72cbc3ce77e7d9c3fbfc17a885276e6d9b226be3
TurtleDoodle1 <muzo17062004@gmail.com>
2026-05-22 22:29:24 +0300

    pulled up to date files from coldat-backend

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "corsheaders",
-
     "rest_framework",
     "corsheaders",
 
@@ -59,7 +57,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
@@ -171,11 +168,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5174",
-]
-CORS_ALLOW_CREDENTIALS = True
